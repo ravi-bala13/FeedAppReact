b909a4686a86ba1b27c47ca1fe233e67a52491c3
Bala Murugan <bals161616@gmail.com>
2023-05-26 15:26:33 +0530

    chat box css changed

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -3,22 +3,40 @@ import PostsList from "./PostsList";
 import Chat from "./Chat";
 import "../css/Home.css";
 import ChatUsers from "./ChatUsers";
+import { useSelector } from "react-redux";
 
 export default function Home() {
   const [recipient, setRecipient] = useState("");
+  const { userId, isLoading } = useSelector((state) => state);
+
+  if (isLoading) {
+    return (
+      <div className="loading-gif">
+        <img
+          src="https://media.tenor.com/hlKEXPvlX48AAAAi/loading-loader.gif"
+          alt=""
+        />
+      </div>
+    );
+  }
+
   return (
-    <div className="home">
-      <div className="left_side_div">
-        <ChatUsers setRecipient={setRecipient} />
-      </div>
+    <div>
+      {userId ? (
+        <div className="home">
+          <div className="left_side_div">
+            <ChatUsers setRecipient={setRecipient} />
+          </div>
 
-      <div className="mid_div">
-        <PostsList />
-      </div>
+          <div className="mid_div">
+            <PostsList />
+          </div>
 
-      <div className="right_side_div">
-        <Chat recipient={recipient} />
-      </div>
+          <div className="right_side_div">
+            <Chat recipient={recipient} />
+          </div>
+        </div>
+      ) : null}
     </div>
   );
 }
--- client/src/components/PostsList.jsx
+++ client/src/components/PostsList.jsx
@@ -1,13 +1,10 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
 import { backendUrl } from "../Constants/Constants";
 import "../css/PostList.css";
 
 function PostsList(props) {
   const [posts, setPosts] = useState([]);
-
-  const { userId, isLoading } = useSelector((state) => state);
 
   useEffect(() => {
     getAllPosts();
@@ -21,41 +18,28 @@ function PostsList(props) {
     }
   };
 
-  if (isLoading) {
-    return (
-      <div className="loading-gif">
-        <img
-          src="https://media.tenor.com/hlKEXPvlX48AAAAi/loading-loader.gif"
-          alt=""
-        />
-      </div>
-    );
-  }
-
   return (
     <div>
-      {userId ? (
-        <div className="postlist_container_box long-box">
-          {posts.map((ele, i) => (
-            <div className="post-container" key={i}>
-              <div className="post-title">
-                <img
-                  src="https://www.kindpng.com/picc/m/495-4952535_create-digital-profile-icon-blue-user-profile-icon.png"
-                  alt=""
-                />
-                {ele.user_id.user_name}
-              </div>
-              <div className="post-box">{ele.content}</div>
-              <div className="bottom-box">
-                <span>Like</span>
-                <span>Dislike</span>
-                <span>Edit</span>
-                <span>Delete</span>
-              </div>
+      <div className="postlist_container_box long-box">
+        {posts.map((ele, i) => (
+          <div className="post-container" key={i}>
+            <div className="post-title">
+              <img
+                src="https://www.kindpng.com/picc/m/495-4952535_create-digital-profile-icon-blue-user-profile-icon.png"
+                alt=""
+              />
+              {ele.user_id.user_name}
             </div>
-          ))}
-        </div>
-      ) : null}
+            <div className="post-box">{ele.content}</div>
+            <div className="bottom-box">
+              <span>Like</span>
+              <span>Dislike</span>
+              <span>Edit</span>
+              <span>Delete</span>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
